2d432355a998bde59f136a12a08c0a62625971ff
Adedapo Aluko <alukoadedapo20@gmail.com>
2024-12-16 09:07:30 -0800

    APIservices

---
+++ AdminConsole1/Services/ApiService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localStorage;
+
+    public ApiService(HttpClient httpClient, ILocalStorageService localStorage)
+    {
+        _httpClient = httpClient;
+        _localStorage = localStorage;
+    }
+
+    public async Task<string> GetDataAsync()
+    {
+        var token = await _localStorage.GetItemAsync<string>("authToken"); // Retrieve the token from local storage
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync("https://localhost:7187");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new HttpRequestException($"Failed to fetch data: {response.ReasonPhrase}");
+            }
+        }
+        else
+        {
+            throw new HttpRequestException("Token is missing");
+        }
+    }
+}
--- ClassLibrary/Models/Department.cs
+++ ClassLibrary/Models/Department.cs
@@ -1,35 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace ClassLibrary.Models
 {
     public class Department
     {
-        public Department()
-        {
-            AdminDepartmentLocations = new List<AdminDepartmentLocation>();
-        }
-
         public int Id { get; set; }
 
         [Required]
         [StringLength(100)]
-        public string Name { get; set; }
+        public string Name { get; set; } // Department name (e.g., "Mental Health", "Bliss")
 
-        [StringLength(500)]
-        public string Description { get; set; }
-        
+        public string Description { get; set; } // Description of the department
         public DateTime DateCreated { get; set; }
-        
-        public int? AgencyId { get; set; }  // Optional
-        public Agency Agency { get; set; }
-        
-        [Required]
-        public int LocationId { get; set; }
-        public Location Location { get; set; }
-
-        // Required navigation property for admin-department-location relationship
+        public int AgencyId { get; set; } // The agency this department belongs to  
+        public Agency Agency { get; set; } // The agency this department belongs to
+        public int LocationId { get; set; } // Foreign key to Location
+        public Location Location { get; set; } // Navigation property
+        public ICollection<Employee> Employees { get; set; }
+        // Many-to-many relationship with NewsItems
+        public ICollection<NewsItemDepartment> NewsItemDepartments { get; set; }
         public ICollection<AdminDepartmentLocation> AdminDepartmentLocations { get; set; }
+        public Department()
+        {
+            Employees = new List<Employee>();
+            NewsItemDepartments = new List<NewsItemDepartment>();
+            AdminDepartmentLocations = new List<AdminDepartmentLocation>();
+        }
+
+
+
     }
+
 }
